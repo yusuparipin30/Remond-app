89cfd5a449a28dcb06a49cc78958142cd724a51e
yusuparipin30 <yusuparipin30@gmail.com>
2021-02-09 19:21:43 +0700

    comit pertama

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => res.json('API is work!'));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server runing on PORT ${PORT}'));
